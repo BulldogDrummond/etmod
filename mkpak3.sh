5bd5d217fb56f0e395ed6b21475629e03046a3e2
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-06 12:53:51 -0500

    Change pak2.pk3 to pak3.pk3

--- mkpak3.sh
+++ mkpak3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -f etmod/pak2.pk3 ]; then
-  rm etmod/pak2.pk3
+if [ -f etmod/pak3.pk3 ]; then
+  rm etmod/pak3.pk3
 fi
 
-cd resource-pak2
+cd resource-pak3
 
-zip -r ../etmod/pak2.pk3 pak2.dat ui/menus.txt ui/assets/*tga \
+zip -r ../etmod/pak3.pk3 pak2.dat ui/menus.txt ui/assets/*tga \
     ui/popup_serverredirect.menu ui/ingame_vote_players.menu \
     ui/mods.menu ui/credits_et260.menu ui/credits_additional.menu \
     ui/main.menu maps/*
