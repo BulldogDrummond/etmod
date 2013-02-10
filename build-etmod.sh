761fbc7c517e5729d3d36618c7ab96dce83c5fef
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-10 06:35:45 -0500

    Revise version code for cmake.

--- build-etmod.sh
+++ build-etmod.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ${1} ]; then
+  if [ ${1} = "new" ]; then
+    BUILD_NUMBER=`cat build.id`
+    NEW_BUILD=`expr $BUILD_NUMBER + 1`
+    echo $NEW_BUILD > build.id
+  fi
+fi
+
 mkdir build
 cd build
 cmake ../
