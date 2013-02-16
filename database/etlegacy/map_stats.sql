2cec5c5273f96a59cb3552e1dde93b9810698e0b
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-16 14:08:20 -0500

    Add RFC 1918 IP Ranges to GeoIP table.  Also includes more sample data for map_stats and player_xp accumulated while testing.

--- database/etlegacy/map_stats.sql
+++ database/etlegacy/map_stats.sql
@@ -42,18 +42,18 @@ CREATE TABLE `map_stats` (
 
 LOCK TABLES `map_stats` WRITE;
 /*!40000 ALTER TABLE `map_stats` DISABLE KEYS */;
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('oasis','0.809497','0.768732',17,'Malone');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('ge_temple_v2a','0.000000','0.925588',17,'Malone');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('goldrush','0.062183','0.973440',22,'Malone');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('radar','0.026536','0.988821',8,'Weissmann [B]');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('crevasse_pe','0.000000','1.000000',0,'');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('navarone','0.098252','0.961830',17,'Cullen');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('trmfght_beta2','0.000000','1.000000',0,'');
+INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('oasis','0.946836','0.729179',17,'Malone');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('et_mor_pro','0.664833','0.866067',26,'Malone');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('citadel_obj','0.130456','0.950948',14,'Cullen');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('battery','0.033226','0.986085',7,'Bauer [B]');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('et_beach','0.155229','0.948700',10,'Fischer [B]');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('crevasse_pe','0.000000','1.000000',0,'');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('braundorf_final','0.030132','0.986877',7,'Keller [B]');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('trmfght_beta2','0.000000','1.000000',0,'');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('ge_temple_v2a','0.000000','0.925588',17,'Malone');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('goldrush','0.062183','0.973440',22,'Malone');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('radar','0.026536','0.988821',8,'Weissmann [B]');
-INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('navarone','0.098252','0.961830',17,'Cullen');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('venice','0.076385','0.968130',16,'Cullen');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('lighthouse','0.019947','0.991067',18,'Cullen');
 INSERT INTO `map_stats` (`map_name`, `rating`, `rating_variance`, `spree_record`, `spree_name`) VALUES ('coast_b1','0.018654','0.991558',5,'Maloney [B]');
